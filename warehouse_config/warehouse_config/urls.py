69edfd1c17948b1a4e55b4adbb36a1b768e48ad8
Naik000024 <quezon.nyko16@gmail.com>
2026-05-13 01:20:10 +0800

    Fix: Add home page view to backend to confirm status

--- warehouse_config/warehouse_config/urls.py
+++ warehouse_config/warehouse_config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Warehouse API Terminal: [ ONLINE ]")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('order.urls')), # This connects your order app to the API
     path('user/', include('user.urls')),
